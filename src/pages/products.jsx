820ae743f2325564ea929902eab934e35d81347e
ronaldiputra09 <emailnyaronal@gmail.com>
2024-04-02 21:19:01 +0700

    feat : rendering list

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,28 +1,42 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    title: "Sepatu Baru",
+    price: "Rp 1.000.000",
+    image: "https://picsum.photos/300/200/?random/1",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque deleniti pariatur omnis odit nihil voluptatibus enim. Laborum incidunt eum impedit rerum! Nulla cumque fugiat aperiam aspernatur provident tenetur, eum dolorem?",
+  },
+  {
+    id: 2,
+    title: "Sepatu Lama",
+    price: "Rp 500.000",
+    image: "https://picsum.photos/300/200/?random/2",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit!",
+  },
+  {
+    id: 3,
+    title: "Sepatu Ada Dong",
+    price: "Rp 2.000.000",
+    image: "https://picsum.photos/300/200/?random/3",
+    description: "Ini adalah sepatu yang sangat bagus sekali!",
+  },
+];
+
 const ProductPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.jpg" />
-        <CardProduct.Body title="Sepatu Baru">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque
-          deleniti pariatur omnis odit nihil voluptatibus enim. Laborum incidunt
-          eum impedit rerum! Nulla cumque fugiat aperiam aspernatur provident
-          tenetur, eum dolorem?
-        </CardProduct.Body>
-        <CardProduct.Footer price="Rp 1.000.000" />
-      </CardProduct>
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.jpg" />
-        <CardProduct.Body title="Sepatu Baru">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque
-          deleniti pariatur omnis odit nihil voluptatibus enim. Laborum incidunt
-          eum impedit rerum! Nulla cumque fugiat aperiam aspernatur provident
-          tenetur, eum dolorem?
-        </CardProduct.Body>
-        <CardProduct.Footer price="Rp 1.000.000" />
-      </CardProduct>
+      {products.map((product) => (
+        <CardProduct>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body title={product.title}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
